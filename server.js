4635bb7ffd92e25b235b9ebaddec0aa33e2c206b
Yayou <78508960+YaYouLeKyou@users.noreply.github.com>
2022-03-24 13:13:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,31 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+let responseObject = {};
+app.get('/api/timestamp/:input', (request, response) => {
+  let input = request.params.input;
+
+  if(input.includes('-')){
+    responseObject['unix']=new Date(input).getTime()
+    responseObject['utc']=new Date(input).toUTCString()
+  }else{
+    input = parseInt(input)
+
+    esponseObject['unix']=new Date(input).getTime()
+    responseObject['utc']=new Date(input).toUTCString()
+  }
+
+  if(!responseObject['unix']||!responseObject['utc']){
+    response.json({ error : "Invalid Date" })
+  }
+  
+  response.json(responseObject)
+})
+
+app.get('/api/timestamp'), (request, response)=>{
+   responseObject['unix']=new Date(input).getTime()
+   responseObject['utc']=new Date(input).toUTCString()
+
+   response.json(responseObject)
+}
